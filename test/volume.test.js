c7d6dbf01586beabba45b93b8f7a095f634d884f
Paul Serby <paul.serby@clock.co.uk>
2012-02-20 14:36:14 +0000

    Refactoring volume testing

--- test/volume.test.js
+++ test/volume.test.js
@@ -13,8 +13,10 @@ function createTemplateFile(content) {
 	return tmpFilename;
 }
 
-module.exports = {
-	'parse 5000 templates': function() {
+describe('volume testing', function() {
+
+	it('parse 200 templates within 2 seconds', function(done) {
+
 		var content = '';
 		for (var i = 0; i < 10000; i += 1) {
 			content +=  'XXXXX #{TOKEN} ZZZZZZZ - ';
@@ -23,15 +25,16 @@ module.exports = {
 		var tmpFilename = createTemplateFile(content);
 
 		var count = 0;
-		async.whilst(function () { return count < 10000; }, function(callback) {
+		async.whilst(function () { return count < 200; }, function(callback) {
 			count++;
 			simplate.parse(tmpFilename, { TOKEN: '12345678'}, function(error, data) {
 				assert.strictEqual(content.length, data.length);
 				callback();
 			});
 		}, function(error) {
-			console.log(count, error);
 			fs.unlink(tmpFilename);
+			done();
 		});
-	}
-};
+	});
+
+});
